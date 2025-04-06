0d84e34a58c4ad37babec0512671e6b5a863a82d
Zerox <zerox@undefined>
2025-04-06 22:09:09 +0800

    [opti] Optimize the spring context tools and add more functions

--- src/main/java/kunlun/spring/SpringAutoConfiguration.java
+++ src/main/java/kunlun/spring/SpringAutoConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 
@@ -32,7 +33,7 @@ public class SpringAutoConfiguration implements ApplicationContextAware, Initial
 
     @Override
     public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
-        SpringUtil.setApplicationContext(applicationContext);
+        SpringUtil.setApplicationContext((ConfigurableApplicationContext) applicationContext);
         log.debug("The application context tools was initialized success. ");
     }
 
