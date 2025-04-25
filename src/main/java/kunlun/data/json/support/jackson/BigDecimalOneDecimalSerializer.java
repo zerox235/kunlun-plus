9a63f658dca4cb55b756210ef0418cf4bb0696ef
Zerox <zerox@undefined>
2025-04-25 23:34:40 +0800

    [feat] Add the jackson serializer and deserializer

---
+++ src/main/java/kunlun/data/json/support/jackson/BigDecimalOneDecimalSerializer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018. the original author or authors.
+ * Kunlun is licensed under the "LICENSE" file in the project's root directory.
+ */
+
+package kunlun.data.json.support.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+public class BigDecimalOneDecimalSerializer extends JsonSerializer<BigDecimal> {
+
+    @Override
+    public void serialize(BigDecimal value, JsonGenerator gen, SerializerProvider p) throws IOException {
+        if (value == null) { gen.writeNull(); return; }
+        gen.writeString(new DecimalFormat("0.0").format(value));
+    }
+
+}
